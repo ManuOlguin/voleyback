d2965039df747daa0f611ffd0bf49d754bc10918
MANUEL OLGUIN <olguin.manuel@maimonidesvirtual.com.ar>
2025-02-20 14:14:46 -0300

    init

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: '*'
+    origin: ['http://localhost:3000', 'https://frontend-voley-analisis.vercel.app']
 }));
 app.use(express.json());
 
